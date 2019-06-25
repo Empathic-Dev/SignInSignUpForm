5253f6c857387e8b06db1d3c0515a6b58104f8f2
Deja <theempathicdev@gmail.com>
2019-06-25 01:00:02 -0400

    project finished!

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Let's finish this before Starbucks closes!");
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => container.classList.add('right-panel-active'));
+signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
